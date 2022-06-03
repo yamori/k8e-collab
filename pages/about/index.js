c0752875a26001a29dc8139c4f4265772743f4d2
Matt Kindzerske <kindzerske@gmail.com>
2022-06-03 14:04:31 -0400

    added SWR client side data fetching, with an api file

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,4 @@
+import useSWR from 'swr'
 import Layout from "../../components/Layout";
 import styles from '../../styles/Home.module.css'
 
@@ -7,7 +8,27 @@ export default function Home() {
       <div className={styles.container}>
         <h1>About Page</h1>
         <p>... just trying to learn some NextJS.</p>
+        <p><b>Goals and motivation:</b></p>
+        <Profile />
       </div>
     </Layout>
+  )
+}
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+function Profile() {
+  const { data, error } = useSWR('../api/profile', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
+  return (
+    <ul>
+      <li>Stack: {data.stack}</li>
+      <li>CI: {data.ci}</li>
+      <li>Hosting: {data.hosting}</li>
+      <li>Product: {data.product}</li>
+    </ul>
   )
 }
